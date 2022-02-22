9920f8b3444a944b4f6049cb85a99ed3bf546517
sandervandersomme <57229133+sandervandersomme@users.noreply.github.com>
2022-02-22 12:32:02 +0100

    Created Classes and function outline

---
+++ test.py
@@ -0,0 +1,33 @@
+from asyncio.tasks import _T1
+
+
+class String:
+    def __init__(self, binary_str) -> None:
+        self.binary_str = binary_str
+
+class Family:
+    def __init__(self, parent1, parent2) -> None:
+        self.parents = [parent1, parent2]
+        self.offspring = []
+    
+    def crossover():
+        pass
+
+class Population:
+    def __init__(self) -> None:
+        self.strings = self.createGen()
+    
+
+    def shuffle(self) -> str:
+        pass
+    
+
+    def createGen(self) -> List[String]:
+        pass
+    
+
+def selection() -> List[String]:
+    pass
+
+
+
